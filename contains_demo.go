0c377782c3eaa2c0d8054ad15b51de4fa085565b
Stuart Henderson <stuarthenderson@stuarts-mbp.home>
2017-06-16 19:40:11 +0100

    improved

--- contains_demo.go
+++ contains_demo.go
@@ -9,30 +9,53 @@ func main() {
 	fmt.Println("Does a value in one slice appear in another?\n .contains() by any other name.\n")
 
 	// Define first slice
-	list_one := []string{"52200", "53233", "54999", "52525"}
+	slice_one := []string{"52200", "53233", "54999", "52525"}
 
 	// Define second slice
-	list_two := []string{"32552", "52200", "43237", "32667"}
+	slice_two := []string{"32552", "52200", "76554", "87765"}
+
+	// Define a third slice which will contain any matches
+	slice_three := []string{}
 
 	// Define a new map to hold values from the first slice.
-	list_one_map := make(map[string]bool)
+	temp_map := make(map[string]bool)
 
 	// Iterate over first slice. Print values and add them to the map.
 	// Just throwing away the index value with "_".
-	for _, v := range list_one {
-		fmt.Println("list one value:", v)
-		list_one_map[v] = true
+	for _, v := range slice_one {
+		fmt.Println("slice one value:", v)
+		temp_map[v] = true
 	}
 
 	// Iterate over the second slice.
 	//
-	for _, v := range list_two {
+	for _, v := range slice_two {
 		fmt.Println("\nSearching for:", v, "in both slices...")
 
-		if list_one_map[v] {
-			fmt.Println("HIT:", v, " is in both slices")
+		if temp_map[v] {
+			fmt.Println(v, "is in both slices")
+			slice_three = append(slice_three, v)
 		} else {
-			fmt.Println("MISS", v, "is not in both slices")
+			fmt.Println(v, "is not in both slices")
+		}
+	}
+	println("\n")
+	// Print out list_three
+	i := len(slice_three)
+
+	if i == 0 {
+		fmt.Println("There were no matches found")
+
+	} else if i == 1 {
+		fmt.Println("There was one match found:")
+		for _, v := range slice_three {
+			fmt.Println("list three value:", v)
+		}
+
+	} else {
+		fmt.Println("There were ", i, "matches found:")
+		for _, v := range slice_three {
+			fmt.Println("list three value:", v)
 		}
 	}
 }
