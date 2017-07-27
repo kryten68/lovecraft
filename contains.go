dc224340f1e1efb861a4f2eedb9e257417e0234f
Stuart Henderson <stuarthenderson@stuarts-mbp.home>
2017-07-27 20:49:53 +0100

    Just an update

--- contains.go
+++ contains.go
@@ -5,23 +5,23 @@ import (
 )
 
 func contains(candidate string) {
-	if Temp[candidate] {
+	if temp[candidate] {
 		fmt.Println("true ", candidate)
 	} else {
 		fmt.Println("false ", candidate)
 	}
 }
 
-var Temp = make(map[string]bool)
+var temp = make(map[string]bool)
 
 func main() {
-	slice_one := []string{"52200", "53233", "54999", "52525"}
-	for _, val := range slice_one {
-		Temp[val] = true
+	sliceOne := []string{"52200", "53233", "54999", "52525"}
+	for _, val := range sliceOne {
+		temp[val] = true
 	}
 
-	slice_two := []string{"42323", "265562", "52200", "62562", "32323", "287878"}
-	for _, val := range slice_two {
+	sliceTwo := []string{"42323", "265562", "52200", "62562", "32323", "287878"}
+	for _, val := range sliceTwo {
 		contains(val)
 	}
 }
