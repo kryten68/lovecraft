d13ae78d4ccaaf0dd8dbe9278510b06921b909ca
Stuart Henderson <stuarthenderson@stuarts-mbp.home>
2017-08-14 22:26:00 +0100

    initial

---
+++ getTimeStringFromMillisSinceEpoch.go
@@ -0,0 +1,29 @@
+// Pass in an int64, a timezome ( "Europe/London" ) and an int relating to the style
+// Needs the time package. Obviously
+func getFormattedTimeFromEpochMillis(z int64, zone string, style int) string {
+	var x string
+	secondsSinceEpoch := z / 1000
+	unixTime := time.Unix(secondsSinceEpoch, 0)
+	timeZoneLocation, err := time.LoadLocation(zone)
+	if err != nil {
+		fmt.Println("Error loading timezone:", err)
+	}
+
+	timeInZone := unixTime.In(timeZoneLocation)
+
+	switch style {
+	case 1:
+		timeInZoneStyleOne := timeInZone.Format("Mon Jan 2 15:04:05")
+		//Mon Aug 14 13:36:02
+		return timeInZoneStyleOne
+	case 2:
+		timeInZoneStyleTwo := timeInZone.Format("02-01-2006 15:04:05")
+		//14-08-2017 13:36:02
+		return timeInZoneStyleTwo
+	case 3:
+		timeInZoneStyleThree := timeInZone.Format("2006-02-01 15:04:05")
+		//2017-14-08 13:36:02
+		return timeInZoneStyleThree
+	}
+	return x
+}
